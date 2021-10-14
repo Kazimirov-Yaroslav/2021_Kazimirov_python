a1cd69d91c4b055fa96ee09c801eff82d97f1044
Kazimirov-Yaroslav <91326850+Kazimirov-Yaroslav@users.noreply.github.com>
2021-10-14 18:49:04 +0300

    Delete ex. 7.py

--- ex. 7.py
+++
@@ -1,9 +0,0 @@
-import turtle
-import math
-turtle.shape('turtle')
-turtle.goto(0.1, 0.1)
-turtle.speed(0)
-k=2/3600
-for i in range (1, 10000):
-    turtle.forward(k*math.sqrt(1+i**2))
-    turtle.left(1)
