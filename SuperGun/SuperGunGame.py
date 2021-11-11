82f45587c18ce167f9c04728671240cab86bb542
Kazimirov-Yaroslav <91326850+Kazimirov-Yaroslav@users.noreply.github.com>
2021-11-11 16:51:47 +0300

    Окончательный рефакторинг

--- SuperGun/SuperGunGame.py
+++ SuperGun/SuperGunGame.py
@@ -22,7 +22,8 @@ class Game:
     def __init__(self):
         self.font2 = pygame.font.Font(None, 100)
         self.end_text = self.font2.render('ИДИ БОТАЙ', True, RED)
-        self.tank = tanks.Tank('tanks_tankGreen_body3.png')
+        self.tank1 = tanks.Tank('tanks_tankGreen_body3.png')
+        self.tank2 = tanks.Tank('tanks_tankGreen_body3.png', center=(700, 500))
         self.airplane = targets.Airplane('airplan_r.png')
         self.bullets = pygame.sprite.Group()
         self.targets = pygame.sprite.Group()
@@ -36,34 +37,44 @@ class Game:
         self.count = str(self.score)
         self.score_text = self.font2.render(self.count, True, RED)
         self.f1 = pygame.font.Font(None, 36)
-        self.tank1 = self.f1.render('Танк 1:', True, 'green3')
+        self.text1 = self.f1.render('Очки:', True, 'black')
+        self.text_tank1 = self.f1.render('Танк 1:', True, 'green3')
+        self.text_tank2 = self.f1.render('Танк 2:', True, 'green3')
 
     def make_text(self):
         """
-        Текст,который появляетсяво время игрового процесса.
+        Текст, который появляетсяво время игрового процесса.
         """
         screen.blit(self.text1, (10, 10))
         screen.blit(self.f1.render(self.count, True, 'black'), (84, 10))
-        screen.blit(self.tank1, (300, 10))
-        screen.blit(self.f1.render(str(self.tank.live), True, 'green3'), (390, 10))
+        screen.blit(self.text_tank1, (270, 10))
+        screen.blit(self.text_tank2, (410, 10))
+        screen.blit(self.f1.render(str(self.tank1.live), True, 'green3'), (360, 10))
+        screen.blit(self.f1.render(str(self.tank2.live), True, 'green3'), (500, 10))
 
     def manual(self):
         """
         Инструкция
         """
-        introduction = self.f1.render('В этой игре вам предстоит сбивать цели с помощью танка', True, 'black')
+        introduction = self.f1.render('В этой игре вам предстоит сбивать цели с помощью 2 танков.', True, 'black')
         screen.blit(introduction, (20, 20))
-        introduction = self.f1.render('У  танка 50 жизней, уклоняйтесь от падающих бомб', True, 'black')
+        introduction = self.f1.render('У каждого танка 50 жизней, уклоняйтесь от падающих бомб.', True, 'black')
         screen.blit(introduction, (20, 50))
         introduction = self.f1.render('Управление:', True, 'black')
         screen.blit(introduction, (20, 80))
-        introduction = self.f1.render('Стрелки вправо и влево - движение танков, их можно зажимать', True, 'black')
+        introduction = self.f1.render('Стрелки вправо и влево - движение первого танка,', True, 'black')
         screen.blit(introduction, (20, 110))
-        introduction = self.f1.render('Space - огонь, можно зажимать для увеличения', True, 'black')
+        introduction = self.f1.render('клавиши X и C - второго, их можно зажимать', True, 'black')
+        screen.blit(introduction, (20, 140))
+        introduction = self.f1.render('Клавиши < и > - управление башней первого танка', True, 'black')
+        screen.blit(introduction, (20, 170))
+        introduction = self.f1.render('клавиши W и S - второго, их можно зажимать.', True, 'black')
         screen.blit(introduction, (20, 200))
-        introduction = self.f1.render('мощности выстрела', True, 'black')
+        introduction = self.f1.render('Огонь: Space - первый танк, Q - второй танк,', True, 'black')
         screen.blit(introduction, (20, 230))
-        introduction = self.f1.render('Для начала игры нажмите крестик', True, 'black')
+        introduction = self.f1.render('можно зажимать для увеличения мощности выстрела.', True, 'black')
+        screen.blit(introduction, (20, 260))
+        introduction = self.f1.render('Для начала игры нажмите крестик.', True, 'black')
         screen.blit(introduction, (20, 300))
 
     def manual_window(self):
@@ -81,82 +92,143 @@ class Game:
                 if event.type == pygame.QUIT:
                     finished = True
 
+    def hit_bullets(self, group, class_group):
+        """
+        Проверка столкновений пули.
+        """
+        hits = pygame.sprite.groupcollide(group, self.bullets, True, True, pygame.sprite.collide_circle)
+        if class_group == "Coin":
+            for _ in hits:
+                c = targets.Coin('star coin 1.png')
+                self.targets.add(c)
+                self.score += 5
+        if class_group == "Airplane":
+            for _ in hits:
+                a = targets.Airplane('airplan_r.png')
+                self.airplanes.add(a)
+                self.score += 10
+
+    def draw(self):
+        """
+        Отрисовка всего.
+        """
+        screen.fill(WHITE)
+        self.count = str(self.score)
+        self.tank1.draw(screen)
+        self.tank2.draw(screen)
+        for b in self.bullets:
+            b.draw(screen)
+        for t in self.targets:
+            t.draw(screen)
+        for a in self.airplanes:
+            a.draw(screen)
+            if a.t == 10:
+                bomb = targets.Bomb('bomb.png', a.rect.x + 110, a.rect.y + 60, FPS)
+                self.bombs.add(bomb)
+        for bomb in self.bombs:
+            bomb.draw(screen)
+
+        self.score_text = self.font2.render("Очков: " + self.count, True, RED)
+        self.make_text()
+
+    def process_events(self):
+        """
+        Обработка событий.
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.tank1.gun.fire2_start()
+                if event.key == pygame.K_q:
+                    self.tank2.gun.fire2_start()
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    new_bullet = self.tank1.gun.fire2_end()
+                    self.bullets.add(new_bullet)
+                if event.key == pygame.K_q:
+                    new_bullet = self.tank2.gun.fire2_end()
+                    self.bullets.add(new_bullet)
+        keys = pygame.key.get_pressed()
+        self.tank1_process(keys)
+        self.tank2_process(keys)
+
+    def tank1_process(self, keys):
+        """
+        Обработка событий первого танка.
+        """
+        if keys[pygame.K_COMMA]:
+            self.tank1.gun.gun_turn('left', screen, self.tank1.get_center_x(), self.tank1.get_center_y())
+        if keys[pygame.K_PERIOD]:
+            self.tank1.gun.gun_turn('right', screen, self.tank1.get_center_x(), self.tank1.get_center_y())
+        if keys[pygame.K_LEFT]:
+            self.tank1.move_l()
+        if keys[pygame.K_RIGHT]:
+            self.tank1.move_r()
+
+    def tank2_process(self, keys):
+        """
+        Обработка событий второго танка.
+        """
+        if keys[pygame.K_x]:
+            self.tank2.gun.gun_turn('left', screen, self.tank2.get_center_x(), self.tank2.get_center_y())
+        if keys[pygame.K_c]:
+            self.tank2.gun.gun_turn('right', screen, self.tank2.get_center_x(), self.tank2.get_center_y())
+        if keys[pygame.K_w]:
+            self.tank2.move_l()
+        if keys[pygame.K_s]:
+            self.tank2.move_r()
+
+    def move(self):
+        """
+        Движение автообЪектов.
+        """
+        for b in self.bullets:
+            b.move()
+        for a in self.airplanes:
+            a.move()
+        for bomb in self.bombs:
+            bomb.move()
+
+    def hit_tanks(self):
+        """
+        Столкновение танков с обЪектами.
+        """
+        for tank in self.tank1, self.tank2:
+            hits = pygame.sprite.spritecollide(tank, self.bombs, True)
+            if hits:
+                tank.live -= 1
+            if tank.live <= 0:
+                return True
+
+    def hit(self):
+        """
+        Проверка всех столкновений.
+        """
+        self.hit_bullets(self.airplanes, "Airplane")
+        self.hit_bullets(self.targets, "Coin")
+        pygame.sprite.groupcollide(self.bombs, self.bullets, True, True)
+        if self.hit_tanks():
+            return True
+
     def mainloop(self):
         """
         Запуск игры
         """
         clock = pygame.time.Clock()
         finished = False
-
         while not finished:
-            screen.fill(WHITE)
-            self.count = str(self.score)
-            self.tank.draw(screen)
-            for b in self.bullets:
-                b.draw(screen)
-            for t in self.targets:
-                t.draw(screen)
-            for a in self.airplanes:
-                a.draw(screen)
-                if a.t == 10:
-                    bomb = targets.Bomb('bomb.png', a.rect.x+110, a.rect.y+60, FPS)
-                    self.bombs.add(bomb)
-            for bomb in self.bombs:
-                bomb.draw(screen)
-            self.make_text()
-
+            self.draw()
             pygame.display.update()
-            keys = pygame.key.get_pressed()
-
             clock.tick(FPS)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    finished = True
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        self.tank.gun.fire2_start()
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_SPACE:
-                        new_bullet = self.tank.gun.fire2_end()
-                        self.bullets.add(new_bullet)
-
-            if keys[pygame.K_x]:
-                self.tank.gun.gun_turn('K_x', screen, self.tank.get_center_x(), self.tank.get_center_y())
-            if keys[pygame.K_z]:
-                self.tank.gun.gun_turn('K_z', screen, self.tank.get_center_x(), self.tank.get_center_y())
-            if keys[pygame.K_LEFT]:
-                self.tank.move_l()
-            if keys[pygame.K_RIGHT]:
-                self.tank.move_r()
-            for b in self.bullets:
-                b.move()
-            for a in self.airplanes:
-                a.move()
-            for bomb in self.bombs:
-                bomb.move()
-            self.tank.gun.power_up()
-
-            hits = pygame.sprite.groupcollide(self.targets, self.bullets, True, True, pygame.sprite.collide_circle)
-            for _ in hits:
-                c = targets.Coin('star coin 1.png')
-                self.targets.add(c)
-                self.score += 5
-            hits = pygame.sprite.groupcollide(self.airplanes, self.bullets, True, True)
-            for _ in hits:
-                a = targets.Airplane('airplan_r.png')
-                self.airplanes.add(a)
-                self.score += 10
-            pygame.sprite.groupcollide(self.bombs, self.bullets, True, True)
-
-            hits = pygame.sprite.spritecollide(self.tank, self.bombs, True)
-            if hits:
-                self.tank.live -= 1
-            if self.tank.live <= 0:
+            if self.process_events():
                 finished = True
-            self.score_text = self.font2.render("Очков: " + self.count, True, RED)
-
-        print(self.score)
-        print(self.tank.live)
+            self.move()
+            self.tank1.gun.power_up()
+            self.tank2.gun.power_up()
+            if self.hit():
+                finished = True
 
     def end_window(self):
         """
--- SuperGun/tanks.py
+++ SuperGun/tanks.py
@@ -7,11 +7,11 @@ WHITE = 0xFFFFFF
 FPS = 30
 
 
-def coord(x, y, length, angel):
+def coord(x, y, length, angle):
     """
     Функция для вычисления координаты пули при выстреле относительнодула пушки.
     """
-    return x+length*math.cos(math.radians(angel)), y-length*math.sin(math.radians(angel))
+    return x+length*math.cos(math.radians(angle)), y-length*math.sin(math.radians(angle))
 
 
 class Gun(pygame.sprite.Sprite):
@@ -24,11 +24,11 @@ class Gun(pygame.sprite.Sprite):
         """
         self.f2_power = 10
         self.f2_on = 0
-        self.angel = 90
-        self.gun_image = load(filename)
-        self.image = self.gun_image
+        self.angle = 90
+        self.gun_image = load(filename).convert_alpha()
+        self.image = self.gun_image.copy()
+        self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
-        self.angel_bul = 0
 
     def fire2_start(self):
         """
@@ -42,10 +42,10 @@ class Gun(pygame.sprite.Sprite):
         Происходит при отпускании кнопки мыши.
         Начальные значения компонент скорости мяча vx и vy зависят от положения мыши.
         """
-        x1, y1 = coord(self.rect.centerx, self.rect.centery, 75, self.angel)
+        x1, y1 = coord(self.rect.centerx, self.rect.centery, 75, self.angle)
         new_bullet = bullet.Bullet('cannon ball.png', x1, y1, FPS)
-        new_bullet.vx = self.f2_power * math.cos(math.radians(self.angel))
-        new_bullet.vy = self.f2_power * math.sin(math.radians(self.angel))
+        new_bullet.vx = self.f2_power * math.cos(math.radians(self.angle))
+        new_bullet.vy = self.f2_power * math.sin(math.radians(self.angle))
         self.f2_on = 0
         self.f2_power = 10
         return new_bullet
@@ -61,19 +61,19 @@ class Gun(pygame.sprite.Sprite):
         В зависимости от клавиши возвращает, как поворачивать пушку или уже нельзя повернуть.
         :param key: Передает, какая клавиша нажата.
         """
-        if 0 < self.angel < 180:
-            if key == 'K_z':
-                self.angel += 5
-            if key == 'K_x':
-                self.angel -= 5
+        if 0 < self.angle < 180:
+            if key == 'left':
+                self.angle += 5
+            if key == 'right':
+                self.angle -= 5
             return True
-        elif self.angel == 180:
-            if key == 'K_x':
-                self.angel -= 5
+        elif self.angle == 180:
+            if key == 'right':
+                self.angle -= 5
             return True
-        elif self.angel == 0:
-            if key == 'K_z':
-                self.angel += 5
+        elif self.angle == 0:
+            if key == 'left':
+                self.angle += 5
             return True
 
     def power_up(self):
@@ -93,9 +93,9 @@ class Gun(pygame.sprite.Sprite):
         :param y: Координаты центра пушки
         """
         if self.targetting(key):
-            self.image = load('vacuum gun.png')
+            self.image = self.gun_image
+            self.image = pygame.transform.rotate(self.image, self.angle)
             self.image.set_colorkey(WHITE)
-            self.image = pygame.transform.rotate(self.image, self.angel)
             self.fix_gun(x, y)
             screen.blit(self.image, self.rect)
 
@@ -104,10 +104,10 @@ class Tank(pygame.sprite.Sprite):
     """
     Класс танка.
     """
-    def __init__(self, filename, *groups):
+    def __init__(self, filename, *groups, center=(100, 500)):
         super().__init__(*groups)
         self.image = load(filename)
-        self.rect = self.image.get_rect(center=(100, 500))
+        self.rect = self.image.get_rect(center=center)
         self.live = 50
         """
         Параметры танка.
